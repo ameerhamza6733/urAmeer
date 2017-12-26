bd150191d00aebc376816925cac8dc2c82cee72f
ameerhamza6733 <ameerhamza6733@gmail.com>
2017-12-26 10:37:08 -0800

    adding sphinx4

--- app/src/main/java/com/ameerhamza6733/okAmeer/assistant/commands/openCamra.java
+++ app/src/main/java/com/ameerhamza6733/okAmeer/assistant/commands/openCamra.java
@@ -1,5 +1,6 @@
 package com.ameerhamza6733.okAmeer.assistant.commands;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
@@ -13,8 +14,9 @@ import com.ameerhamza6733.okAmeer.assistant.CommandModel;
 public class openCamra implements Command {
     @Override
     public void execute(CommandModel commandModel) {
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        Intent intent = new Intent("org.tensorflow.demo");
         commandModel.getContext().startActivity(intent);
+        launchComponent("org.tensorflow.demo","ClassifierActivity",commandModel.getContext());
     }
 
     @Override
@@ -26,4 +28,14 @@ public class openCamra implements Command {
     public String getTtsPhrase(Context context) {
         return null;
     }
+
+    private void  launchComponent(String packageName, String name,Context context){
+        Intent launch_intent = new Intent("android.intent.action.MAIN");
+        launch_intent.addCategory("android.intent.category.LAUNCHER");
+        launch_intent.setComponent(new ComponentName(packageName, name));
+        launch_intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        context.startActivity(launch_intent);
+    }
+
 }
